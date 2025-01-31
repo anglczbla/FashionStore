61d85b93bd66cf9abf382a847b74a14c3791f934
angel <angelicaizabella90@gmail.com>
2025-01-31 16:42:49 +0700

    push spa-angular

--- spa-angular/src/app/app.routes.ts
+++ spa-angular/src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { OrdersComponent } from './components/orders/orders.component';
 import { ProductsComponent } from './components/products/products.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { ShippingComponent } from './components/shipping/shipping.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'orders',  component: OrdersComponent},
-    { path: 'products',  component: ProductsComponent},
-    { path: 'payment',  component: PaymentComponent},
-    { path: 'shipping',  component: ShippingComponent},
+    { path: 'orders',  component: OrdersComponent, canActivate: [AuthGuard] },
+    { path: 'products',  component: ProductsComponent, canActivate: [AuthGuard] },
+    { path: 'payment',  component: PaymentComponent, canActivate: [AuthGuard] },
+    { path: 'shipping',  component: ShippingComponent, canActivate: [AuthGuard] },
+    { path: 'auth', component: AuthComponent },
+    { path: '**', redirectTo: 'auth' },
     
 
     
