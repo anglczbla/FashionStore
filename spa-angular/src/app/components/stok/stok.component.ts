ce35b8e5c482dbd0d70001fe0a995c460aa778f2
fitriaulianisa <aulianisafitri434@gmail.com>
2025-04-21 22:21:18 +0700

    update

---
+++ spa-angular/src/app/components/stok/stok.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stok',
+  imports: [],
+  templateUrl: './stok.component.html',
+  styleUrl: './stok.component.css'
+})
+export class StokComponent {
+
+}
