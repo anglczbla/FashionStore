4d0eeb52810a8054d384f5061c6cc57986998b5b
fitriaulianisa <aulianisafitri434@gmail.com>
2025-04-24 12:35:21 +0700

    update routes riview

--- app_api/routes/review.js
+++ app_api/routes/review.js
@@ -4,43 +4,18 @@ const reviewController = require("../controllers/reviewController");
 
 
 // Route untuk mendapatkan semua testimoni
-// router.get("/", reviewController.getAllReview);
-
-// // Route untuk mendapatkan review berdasarkan ID
-// router.get("/:id", reviewController.getReviewById);
-
-// // Route untuk membuat review baru
-// router.post("/", reviewController.createReview);
-
-// // Route untuk memperbarui review berdasarkan ID
-// router.put("/:id", reviewController.updateReview);
-
-// // Route untuk menghapus review berdasarkan ID
-// router.delete("/:id",reviewController.deleteReview);
-
-// Definisi rute untuk produk
 router.get("/", reviewController.getAllReview);
-router.post(
-  "/",
-  authMiddleware,
-  roleMiddleware("admin"),
-  reviewController.createReview
-);
 
+// Route untuk mendapatkan review berdasarkan ID
 router.get("/:id", reviewController.getReviewById);
-router.put(
-  "/:id",
-  authMiddleware,
-  roleMiddleware("admin"),
- 
-  reviewController.updateReview
-);
 
-router.delete(
-  "/:id",
-  authMiddleware,
-  roleMiddleware("admin"),
-  reviewController.deleteReview
-);
+// Route untuk membuat review baru
+router.post("/", reviewController.createReview);
+
+// Route untuk memperbarui review berdasarkan ID
+router.put("/:id", reviewController.updateReview);
+
+// Route untuk menghapus review berdasarkan ID
+router.delete("/:id",reviewController.deleteReview);
 
 module.exports = router;
