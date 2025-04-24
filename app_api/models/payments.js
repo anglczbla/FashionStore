d83ea4900dd163b9ae45786307a17f64140df6b3
angel <angelicaizabella90@gmail.com>
2025-04-24 12:57:48 +0700

    payment testing

--- app_api/models/payments.js
+++ app_api/models/payments.js
@@ -6,7 +6,7 @@ const paymentSchema = new mongoose.Schema({
     // ID referensi ke produk yang dibeli
     orders_id: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Products",
+        ref: "orders",
         required: true,
     },
     // Jumlah pembayaran
