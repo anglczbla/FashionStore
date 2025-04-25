609617c8ace4f6b2d5e475b3853c36b8ff0354ee
angel <angelicaizabella90@gmail.com>
2025-04-25 16:34:14 +0700

    home logo

--- spa-angular/src/app/app.component.ts
+++ spa-angular/src/app/app.component.ts
@@ -1,37 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
-import { RouterModule } from '@angular/router'; 
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  standalone: true,
-  imports:[
-    RouterOutlet,
-    HomeComponent,
-    RouterModule
-  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  standalone:true
 })
 export class AppComponent {
   title = 'spa-angular';
-  isLoggedIn: boolean = false; // Menyimpan status login
-  constructor(private router: Router) { } // Menambahkan router pada konstruktor
-  
-  ngOnInit() {
-    // Memeriksa apakah ada token di localStorage
-    this.isLoggedIn = !!localStorage.getItem('authToken');
-  }
-
-  onLogout() {
-    // Menghapus token dari localStorage saat logout
-    localStorage.removeItem('authToken');
-    this.isLoggedIn = false; // Mengubah status login menjadi false
-    this.router.navigate(['/auth']); // Arahkan ke halaman login setelah logout
-  }
 }
-
-
-
--- spa-angular/src/app/components/home/home.component.ts
+++ spa-angular/src/app/components/home/home.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class HomeComponent {
 
